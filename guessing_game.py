a51af1164911b483848cd9cf4e0c7948f36b60ee
manikandans77 <142792416+manikandans77@users.noreply.github.com>
2025-08-13 08:28:17 +0530

    getting input from user

--- guessing_game.py
+++ guessing_game.py
@@ -2,3 +2,6 @@
 print("G U E S S I N G    G A M E")
 print("--------------------------")
 print("Please think of a number (between 1 and 5) ")
+
+# Step 2  - interactivity, getting input from user
+input("Press enter when you are ready!...")
